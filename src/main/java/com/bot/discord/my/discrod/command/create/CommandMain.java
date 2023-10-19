fcb8ebb7c62ca84d1e486dc0d81f50922e4e02a9
igor <igor.kostenko.1999@mail.ru>
2023-10-19 23:58:23 +0300

    write some idea, for refactor command

---
+++ src/main/java/com/bot/discord/my/discrod/command/create/CommandMain.java
@@ -0,0 +1,28 @@
+package com.bot.discord.my.discrod.command.create;
+
+import discord4j.core.event.domain.message.MessageCreateEvent;
+import lombok.RequiredArgsConstructor;
+import reactor.core.publisher.Mono;
+
+@RequiredArgsConstructor
+public abstract class CommandMain<T> implements CommandI {
+
+    private final CommandDefinition<T> commandDefinition;
+    private final CreateHandler<T> createHandler;
+    private final CommandResolver<T> commandResolver;
+
+    public String getCommand() {
+        return commandDefinition.getCommand();
+    }
+
+    public boolean isActive() {
+        return commandDefinition.isActive();
+    }
+
+    public Mono<Void> execute(MessageCreateEvent event, String commandLine) {
+        var dto = commandResolver.resolve(commandLine);
+
+        return createHandler.executeCommand(event, dto);
+    }
+
+}
