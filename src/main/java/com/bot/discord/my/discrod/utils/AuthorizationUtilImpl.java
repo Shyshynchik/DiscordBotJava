c43a297dabca108b2d725290e740521636ef1682
igor <igor.kostenko.1999@mail.ru>
2023-10-20 23:00:09 +0300

    extract isAdmin method

---
+++ src/main/java/com/bot/discord/my/discrod/utils/AuthorizationUtilImpl.java
@@ -0,0 +1,17 @@
+package com.bot.discord.my.discrod.utils;
+
+import discord4j.core.object.entity.Message;
+import discord4j.core.object.entity.PartialMember;
+import discord4j.rest.util.Permission;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+@Component
+public class AuthorizationUtilImpl implements AuthorizationUtil {
+    @Override
+    public Mono<Boolean> isAdmin(Message message) {
+        return message.getAuthorAsMember()
+                .flatMap(PartialMember::getBasePermissions)
+                .map(permissions -> permissions.contains(Permission.MANAGE_MESSAGES));
+    }
+}
